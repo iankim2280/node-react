9b3921f651f0370fed0eb756c0367fd3eccc1466
IanKim <iankim2280@gmail.com>
2020-09-24 15:54:32 -0400

    generate some folders and removed unnecessary features

---
+++ client/src/components/view/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function LandingPage() {
+//   return <div></div>;
+// }
+const LandingPage = () => {
+  return <div>Landing Page</div>;
+};
+export default LandingPage;
---
+++ client/src/components/view/LoginPage/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const LoginPage = () => {
+  return <div>Login Page</div>;
+};
+// rfac = generate an arrow function and export right away
+// rface = generate an arrow function and export separately
