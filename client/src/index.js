ca1998f1c85aadd3efafe809ff74cde98821a277
IanKim <iankim2280@gmail.com>
2020-09-25 15:19:17 -0400

    added redux thunk but not completed

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { promiseMiddleware } from "redux-promise";
-// import { ReduxThunk } from "redux-thunk";
+import { ReduxThunk } from "redux-thunk";
 import Reducer from "./_reducers";
 
 // normal store can accept only objects, so applied promiseMiddleware, ReduxThunk so that they can accpet promise and functions.
