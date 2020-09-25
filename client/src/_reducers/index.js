b398f86a4e7a09d1fa0235cb89ad8eaae1781a45
IanKim <iankim2280@gmail.com>
2020-09-25 15:14:14 -0400

    installed redux series

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  // user
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { promiseMiddleware } from "redux-promise";
+import { ReduxThunk } from "redux-thunk";
+import Reducer from "./_reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// normal store can accept only objects, so applied promiseMiddleware, ReduxThunk so that they can accpet promise and functions.
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+// Redux (Strict unidirectional data flow)
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
