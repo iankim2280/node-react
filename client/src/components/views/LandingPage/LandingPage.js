666047cb581e9b03032bb756a603f84eada28ee9
IanKim <iankim2280@gmail.com>
2020-09-25 15:45:18 -0400

    using hooks, compare to class components

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,32 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
-// import { response } from "express";
 
 const LandingPage = () => {
+  const [Name, setName] = useState("");
+  // using Hook
   useEffect(() => {
-    axios.get("/api/hello").then((res) => console.log(res));
+    // axios.get("/api/hello").then((res) => console.log(res));
     // console.log(res.data));
+
+    axios.get("/api/hello").then((res) => setName(res.data.name));
   }, []);
-  return <div>Landing Page!!</div>;
+  return <div>Landing Page!!{name}</div>;
 };
 export default LandingPage;
+
+// // using class component
+// export default class LandingPage extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: "" };
+//   }
+//   // almost the same with useEffect
+//   componentDidMount() {
+//     axios.get("/api/hello").then((res) => {
+//       this.setState({ name: res.data.name });
+//     });
+//   }
+//   render() {
+//     return <div>My name is {this.state.name}</div>;
+//   }
+// }
